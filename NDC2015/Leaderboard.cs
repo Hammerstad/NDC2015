f9e397b82125f0a425a62df10c2bea43ddf5a4b0
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-09-30 15:15:29 +0200

    Update leaderboard to show logo and only top 10 results.

--- NDC2015/Leaderboard.cs
+++ NDC2015/Leaderboard.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Text;
-using System.Windows.Documents;
-
 
 namespace NDC2015
 {
@@ -11,25 +9,67 @@ namespace NDC2015
 
         public string CreateHTML(List<Score> scores, int latestContestant = 0)
         {
-            const string Header = "<html><head><META HTTP-EQUIV=\"refresh\" CONTENT=\"10\"/><link rel=\"stylesheet\" type =\"text / css\" href =\"leaderboard.css\" media =\"screen\" /></head><body><h1>Leaderboard</h1><table style = \"width:100%\">";
-            var body = new StringBuilder();
-            for (int i = 0; i < scores.Count; i++)
+            int numberOfRowsToDisplay = scores.Count >= 10 ? 10 : scores.Count;
+            var html = new StringBuilder(GetHtmlHeaderAndStartOfBody());
+
+            AddRowsInHtmlTable(html, scores, latestContestant, numberOfRowsToDisplay);
+            AddLatestContestantIfBelowHighScoreList(html, scores, latestContestant);
+
+            html.Append(GetHtmlFooter());
+            return html.ToString();
+        }
+
+        private static void AddRowsInHtmlTable(StringBuilder stringBuilder, IReadOnlyList<Score> scores, int latestContestant, int numberOfRowsToDisplay)
+        {
+            for (int i = 0; i < numberOfRowsToDisplay; i++)
             {
-                var position = i + 1;
-                string rowClass = position == latestContestant ? "id=\"selected\"" : "";
-                var score = scores[i];
-                body.Append("<tr ");
-                body.Append(rowClass);
-                body.Append("><td align=\"right\" style=\"width: 80px\">");
-                body.Append(position);
-                body.Append(".</td><td>");
-                body.Append(score.ContestantName);
-                body.Append("</td><td align=\"center\" style=\"width: 250px\">");
-                body.Append(score.FriendlyElapsedTime);
-                body.AppendLine("</td></tr>");
+                AddRowInHtmlTable(stringBuilder, scores, latestContestant, i + 1);
             }
-            const string Footer = "</table></body></html>";
-            return Header + body + Footer;
-        } 
+        }
+
+        private static void AddLatestContestantIfBelowHighScoreList(StringBuilder stringBuilder, IReadOnlyList<Score> scores, int latestContestant)
+        {
+            if (latestContestant > 10)
+            {
+                AddRowInHtmlTable(stringBuilder, scores, latestContestant, latestContestant);
+            }
+        }
+
+        private static void AddRowInHtmlTable(StringBuilder stringBuilder, IReadOnlyList<Score> scores, int latestContestant, int position)
+        {
+            string rowClass = position == latestContestant ? "id=\"selected\"" : "";
+            var score = scores[position-1];
+            stringBuilder.Append("<tr ");
+            stringBuilder.Append(rowClass);
+            stringBuilder.Append("><td align=\"right\" style=\"width: 80px\">");
+            stringBuilder.Append(position);
+            stringBuilder.Append(".</td><td>");
+            stringBuilder.Append(score.ContestantName);
+            stringBuilder.Append("</td><td align=\"center\" style=\"width: 250px\">");
+            stringBuilder.Append(score.FriendlyElapsedTime);
+            stringBuilder.AppendLine("</td></tr>");
+        }
+
+        private static string GetHtmlHeaderAndStartOfBody()
+        {
+            const string HtmlHeader =
+                @"<html>
+                    <head>
+                        <META HTTP-EQUIV=""refresh"" CONTENT=""10""/>
+                        <link rel=""stylesheet"" type =""text / css"" href =""leaderboard.css"" media =""screen"" />
+                    </head>";
+            const string HtmlStartOfBody = 
+                @"<body>
+                    <h1>
+                        Leaderboard <img src=""logo.png"" align=""right"" height=""150"">
+                    </h1>
+                    <table style = ""width:100%"">";
+            return HtmlHeader + HtmlStartOfBody;
+        }
+
+        private static string GetHtmlFooter()
+        {
+            return "</table></body></html>";
+        }
     }
 }
