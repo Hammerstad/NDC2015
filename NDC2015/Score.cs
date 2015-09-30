6122dc6385dec2c67cb3d44a7d06099b98e8769b
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-09-30 15:50:20 +0200

    Make Score.cs immutable.
    Call to default constructor no longer needed for C# versions below 6.0.

--- NDC2015/Score.cs
+++ NDC2015/Score.cs
@@ -5,16 +5,16 @@ namespace NDC2015
     [Serializable]
     public struct Score
     {
-        public Score(TimeSpan elapsedTime, string contestantName, string phone) : this()
+        public Score(TimeSpan elapsedTime, string contestantName, string phone)
         {
             ElapsedTime = elapsedTime;
             ContestantName = contestantName;
             Phone = phone;
         }
 
-        public TimeSpan ElapsedTime { get; set; }
-        public string ContestantName { get; set; }
-        public string Phone;
+        public readonly TimeSpan ElapsedTime;
+        public readonly string ContestantName;
+        public readonly string Phone;
 
         public string FriendlyElapsedTime
         {
